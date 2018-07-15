ea71c803b72d9d656d0b07385cce79ffb1c66f81
ParhamNikdouz <pnikdouz@gmail.com>
2018-07-16 00:46:19 +0430

    Create Controllers

---
+++ app/Http/Controllers/UserWallet/UserWalletController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\UserWallet;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserWalletController extends Controller
+{
+    //
+}
